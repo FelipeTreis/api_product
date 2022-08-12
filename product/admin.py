cecf488efc629cbe9da3a2828645bced3a5155a6
Felipe Treis <felipetreis1@gmail.com>
2022-08-12 15:26:27 -0300

    Created Admin classes

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Category, Product, Provider
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cnpj',)
+    list_display_links = ('name', 'cnpj',)
+    list_filter = ('name', 'cnpj',)
+    list_editable = ('is_active',)
+    list_per_page = 15
+    search_fields = ('name', 'cnpj',)
+    ordering = ('-id',)
+    prepopulated_fields = {"slug": ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+    list_display_links = ('category',)
+    list_filter = ('category',)
+    list_editable = ('is_active',)
+    list_per_page = 15
+    search_fields = ('category',)
+    ordering = ('-id',)
+    prepopulated_fields = {"slug": ('category',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category',)
+    list_display_links = ('name',)
+    list_filter = ('name', 'category', 'provider',)
+    list_editable = ('is_active',)
+    list_per_page = 15
+    search_fields = ('name', 'category',)
+    ordering = ('-id',)
+    prepopulated_fields = {"slug": ('name',)}
