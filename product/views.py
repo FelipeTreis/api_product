b8c9e15e53a38e1070c32ffcc3f3b4cd2257e07c
Felipe Treis <felipetreis1@gmail.com>
2022-08-15 22:30:11 -0300

    Created templates and styles

--- product/views.py
+++ product/views.py
@@ -1,9 +1,14 @@
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from product.models import Category, Product, Provider
 from product.serializers import (CategorySerializer, ProductSerializer,
                                  ProviderSerializer)
+
+
+def home(request):
+    return render(request, 'product/pages/home.html')
 
 
 @api_view()
